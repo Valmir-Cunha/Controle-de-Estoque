27b5684eb99d279fc847d9a0893defddf58ddd24
AlissonnR <85799112+AlissonnR@users.noreply.github.com>
2021-06-18 21:10:57 -0300

    Update GestorDeEstoque.java

--- src/entidades/GestorDeEstoque.java
+++ src/entidades/GestorDeEstoque.java
@@ -1,8 +1,10 @@
 package entidades;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class GestorDeEstoque extends Funcionario {
 	private int codigoGestorEstoque;
@@ -14,20 +16,27 @@ public class GestorDeEstoque extends Funcionario {
 	}
 
 	public void cadastrarProdutos(Produto produto) throws IOException {
-		//Procurar se produto jja existe pelo código
-		FileWriter fw = new FileWriter ("dados\\Produtos.txt",true);
-        PrintWriter pw = new PrintWriter(fw);
-        pw.println("Codigo: "+produto.getCodigoProduto());
-        pw.println("Nome: "+produto.getNome());
-        pw.println("Marca: "+produto.getMarca());
-        pw.println("Preço: "+produto.getPreco());
-        pw.println("Quantidade: "+produto.getQuantidadeEstoque());
-        pw.println("Categoria: "+produto.getCategoria());
-        pw.println("----------------");
-        pw.flush();
-        pw.close();
-        fw.close();
-	}
+            Scanner inteiro = new Scanner(System.in);
+            String nome;
+            System.out.println("Qual nome do produto?");
+            nome = inteiro.next();
+            File fw = new File (nome+(".txt")); //Verifica se produto já está cadastrado.
+            if (fw.exists()) {
+                System.out.println("O produto já está cadastrado");
+                
+            } else { //Cadastrado o produto e faz o arquivo dele se o produto não estiver cadastrado.
+                PrintWriter pw = new PrintWriter(fw);
+                pw.println("Codigo: "+produto.getCodigoProduto());
+                pw.println("Nome: "+produto.getNome());
+                pw.println("Marca: "+produto.getMarca());
+                pw.println("Preço: "+produto.getPreco());
+                pw.println("Quantidade: "+produto.getQuantidadeEstoque());
+                pw.println("Categoria: "+produto.getCategoria());
+                pw.println("----------------");
+                pw.flush();
+                pw.close();
+        }
+        }
 	
 	public void excluirProdutos(Produto produto) {
 		
