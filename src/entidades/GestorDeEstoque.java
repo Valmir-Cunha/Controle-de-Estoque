bb8cd15a50ead0cf1db4e76b0a1e693541c4bc8f
AlissonnR <85799112+AlissonnR@users.noreply.github.com>
2021-06-29 21:37:09 -0300

    Update GestorDeEstoque.java

--- src/entidades/GestorDeEstoque.java
+++ src/entidades/GestorDeEstoque.java
@@ -2,6 +2,9 @@ package entidades;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -123,6 +126,13 @@ public class GestorDeEstoque extends Funcionario {
 			System.out.println("Categoria nao existe/ nao encontrada");
 		}
 	}
+        public void salvar() throws FileNotFoundException, IOException{
+            try (FileOutputStream f = new FileOutputStream(new File("\\dados\\Produtos.txt"),true)) {
+                ObjectOutputStream o = new ObjectOutputStream(f);
+                o.writeObject(estoque.getProdutosCadastrados().toString());
+                o.close();
+            }
+       }
 	
 	/*
 	public void cadastraCategoria(Produto produto) throws IOException{
