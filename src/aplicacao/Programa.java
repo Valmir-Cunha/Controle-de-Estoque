87eb5123e06d361526c450f7855d69e094764ba1
Valmir-Cunha <vinicius.git36@gmail.com>
2021-06-30 23:25:40 -0300

    Update jLogin.java e JVenda.java

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -5,23 +5,26 @@ import entidades.Categoria;
 import entidades.GestorDeEstoque;
 import entidades.Produto;
 import entidades.Vendedor;
+import formularios.JLogin;
 import servicos.Administracao;
 import servicos.Estoque;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Estoque estoqueMercado = new Estoque(); 
-		Administracao admMercado = new Administracao();
-		GestorDeEstoque gestor = new GestorDeEstoque("vinicius","rua123",9993242,1,"vinicius192","vinicius",900.0,estoqueMercado);
-		Arquivos arq = new Arquivos(estoqueMercado, admMercado);
-		//Categoria c = new Categoria("sfg",1);
-		//Produto pp = new Produto(1,"biscoitto","bno",2.5,5,c);
-		//System.out.println(pp.toString());
-		gestor.cadastrarProdutos();
-		System.out.println("Www");
-		gestor.getEstoque().exibirProdutosCadastrados();
-		Vendedor vinicius = new Vendedor("vinicius","rua1",79991343,123,"vinii","senha",34567.0);
-		
+            Estoque estoqueMercado = new Estoque(); 
+            Administracao admMercado = new Administracao();
+            Arquivos arq = new Arquivos(estoqueMercado, admMercado);
+            JLogin login = new JLogin(admMercado);
+            login.setVisible(true);
+            /*GestorDeEstoque gestor = new GestorDeEstoque("vinicius","rua123",9993242,1,"vinicius192","vinicius",900.0,estoqueMercado);
+            //Categoria c = new Categoria("sfg",1);
+            //Produto pp = new Produto(1,"biscoitto","bno",2.5,5,c);
+            //System.out.println(pp.toString());
+            gestor.cadastrarProdutos();
+            System.out.println("Www");
+            gestor.getEstoque().exibirProdutosCadastrados();
+            Vendedor vinicius = new Vendedor("vinicius","rua1",79991343,123,"vinii","senha",34567.0);
+            */
 	}
 }
--- src/servicos/Caixa.java
+++ src/servicos/Caixa.java
@@ -7,8 +7,5 @@ public class Caixa {
     Estoque estoque;
     Vendas venda;
 	
-    public void iniciarVenda() {
-	Cliente cliente = new Cliente();
-	//Vendas venda = new Vendas(cliente,);
-	}
+   
 }
