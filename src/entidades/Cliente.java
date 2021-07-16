f9a0d833cdbba59cba26b1cca25fb04a4e40ecaa
AlissonnR <85799112+AlissonnR@users.noreply.github.com>
2021-07-15 23:17:04 -0300

    Update Cliente.java

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -39,7 +39,7 @@ public class Cliente extends Pessoa{
 
         @Override
     public String toString() {
-          return ";" + nome
+          return "" + nome
                            +";" + numeroTelefone 
                            +";" + endereco
                            +";" + codigoCliente
